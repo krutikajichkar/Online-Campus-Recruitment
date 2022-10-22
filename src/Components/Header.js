ad6f64553df8501419bcf06969530006772f0e81
Krutika Jichkar <93243210+krutikajichkar@users.noreply.github.com>
2022-10-22 18:46:42 +0530

    .

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,17 +1,18 @@
-import React from 'react'
-import './Header.css'
-import NaacLogo from '../Components/Images/NaacLogo.png'
-import tgpcetLogo from '../Components/Images/tgpcetLogo.png'
-
+import React from "react";
+import "./Header.css";
+import NaacLogo from "../Components/Images/NaacLogo.png";
+import tgpcetLogo from "../Components/Images/tgpcetLogo.png";
 
 function NavHome() {
   return (
-    <div id='header-div'>
-      <img id='tgpcetlogo' src={tgpcetLogo} alt="tgpcetLogo" />
-      <img id='naaclogo' src={NaacLogo} alt="NaacLogo" />
-     
+    <div>
+      <div id="header-div">
+        <img id="tgpcetlogo" src={tgpcetLogo} alt="tgpcetLogo" />
+        <img id="naaclogo" src={NaacLogo} alt="NaacLogo" />
+      </div>
+      
     </div>
-  )
+  );
 }
 
-export default NavHome
+export default NavHome;
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,21 +1,13 @@
-import React from 'react'
-import './Navbar.css'
+import React from "react";
+//import "./Navbar.css";
 function Navbar() {
   return (
     <div>
-       <nav>
-       <ul type='none'>
-            <li>Home</li>
-            <li>About</li>
-            <li>Alumni Activities</li>
-            <li>Alumni Committee</li>
-            <li>Feedback</li>
-            <li>Register</li>
-            <li>Login</li>
-        </ul>
-       </nav>
+        <div>
+          
+        </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
