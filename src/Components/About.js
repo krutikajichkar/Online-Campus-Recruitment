b5d5b03a77d24cb52c9315d40cde769065a0706b
Ritik0706 <ritiklg76@gmail.com>
2022-10-26 11:53:13 +0530

    .

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-//import './About.css';
+import './About.css';
 import Header from './Header'
 import Navbar from './Navbar'
 import Footer from './Footer'
@@ -29,7 +29,7 @@ function About() {
                     </div>
                 </div>
             </div>
-            <Footer/>
+            <Footer />
         </div>
     )
 }
