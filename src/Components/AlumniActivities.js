3ec10cb7c8ff7c0d1ca490716afe88264d6bfde8
Ritik0706 <ritiklg76@gmail.com>
2022-10-26 22:01:38 +0530

    Created file Alumni Activities

--- src/Components/AlumniActivities.js
+++ src/Components/AlumniActivities.js
@@ -1,12 +1,124 @@
+// import { AccordionSummary } from '@mui/material';
 import React from 'react'
+import './AlumniActivities.css';
 import Header from './Header'
 import Navbar from './Navbar'
 
 function AlumniActivities() {
   return (
     <div>
-        <Header/>
-        <Navbar/>
+      <Header />
+      <Navbar />
+
+      <div class="col-6">
+        <div class="Th-1">
+          <div class="Alumni-img">
+            <img src="http://tgpcet.com/assets/img/Faculty/Jayant_Rohankar.jpg" class="Jayantsir-img" alt="Alumni-Incharge" />
+          </div>
+          <h2 id="Incharge-Name">MR. JAYANT ROHANKAR</h2>
+          <p id="Incharge">Amlumni Incharge</p>
+          <p id="Incharge-Num"> 85549 82302</p>
+          <p id="Incharge-Id"> jayant.mtech@tgpcet.com </p>
+          <div class="Alumni-line"></div>
+          <ul class="AlumniSocial-Icons">
+            <li class="facebook">
+              <a><i class="fab fa-facebook-f"></i></a>
+            </li>
+            <li class="youtube">
+              <a><i class="fab fa-youtube"></i></a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <details class="Alumni-panel">
+        <summary> Alumni Meet (Nostalgia)</summary>
+        <h3 class="panel-heading">Tulsiramji Gaikwad-Patil College of Engineering & Technology Alumni Association Registration Number:- MAH 701/14(Nag)</h3>
+        <p class="panel-detail"> Nostalgia is the flagship event of the Alumni association of TGPCET. It is the annual Alumni Meet - an evocative event that marks homecoming, celebration and unparalleled joy of communion with the alma mater. It is an ideal opportunity to renew old bonds, foster new ties and relive the magical moments all over again. The Alumnus series is a new initiative launched, which provides an exciting platform for regular interaction between the current students & the alumni of TGPCET, facilitated by Guest Lectures, Technical Events, Workshops, etc.</p>
+        <p class="panel-detail">TusiramjiGaikwad-Patil College of Engineering & Technology Alumni network brings together graduates of a variety of programs right from its inception. The collective strength of this alumni network is the backbone of TGPCET. The alumni association acts as a bridge between the illustrious alumni and their alma mater, giving the current students and the alumni a chance to develop positive synergies and grow together in this process. The strength of any institute is comprises the present students, who are the future leaders and the alumni of the college, who bring in accolades by virtue of their success and standing in the society. It gives an opportunity for the present, the past and the incoming batch to interact with each other and learn from shared experiences.</p>
+      </details>
+
+      <details class="Alumni-panel2">
+        <summary> Glimpses of Online Alumni Meet 2019-20 </summary>
+        <div class="panel2-row">
+          <div class="panel2-imgbox">
+            <div class="thumbnail">
+              <img src="http://tgpcet.com/assets/img/ALUMNI/Alumni-Meet-2019-20/1.jpg" class="panel2-img" alt="img" />
+            </div>
+          </div>
+          <div class="panel2-imgbox">
+            <div class="thumbnail">
+              <img src="http://tgpcet.com/assets/img/ALUMNI/Alumni-Meet-2019-20/2.jpg" class="panel2-img" alt="img" />
+            </div>
+          </div>
+          <div class="panel2-imgbox">
+            <div class="thumbnail">
+              <img src="http://tgpcet.com/assets/img/ALUMNI/Alumni-Meet-2019-20/3.jpg" class="panel2-img" alt="img" />
+            </div>
+          </div>
+        </div>
+      </details>
+
+
+      <details class="Alumni-panel2">
+        <summary>  Alumni Meet at Pune (Nostalgia 2018) </summary>
+        <div class="panel2-row">
+          <div class="panel2-imgbox">
+            <div class="thumbnail">
+              <img src="http://tgpcet.com/assets/img/ALUMNI/Alumni-Meet-Pune/1.jpg" class="panel2-img" alt="img" />
+            </div>
+          </div>
+          <div class="panel2-imgbox">
+            <div class="thumbnail">
+              <img src="http://tgpcet.com/assets/img/ALUMNI/Alumni-Meet-Pune/2.jpg" class="panel2-img" alt="img" />
+            </div>
+          </div>
+          <div class="panel2-imgbox">
+            <div class="thumbnail">
+              <img src="http://tgpcet.com/assets/img/ALUMNI/Alumni-Meet-Pune/3.jpg" class="panel2-img" alt="img" />
+            </div>
+          </div>
+          <div class="panel2-imgbox">
+            <div class="thumbnail">
+              <img src="http://tgpcet.com/assets/img/ALUMNI/Alumni-Meet-Pune/4.jpg" class="panel2-img" alt="img" />
+            </div>
+          </div>
+          <div class="panel2-imgbox">
+            <div class="thumbnail">
+              <img src="http://tgpcet.com/assets/img/ALUMNI/Alumni-Meet-Pune/5.jpg" class="panel2-img" alt="img" />
+            </div>
+          </div>
+          <div class="panel2-imgbox">
+            <div class="thumbnail">
+              <img src="http://tgpcet.com/assets/img/ALUMNI/Alumni-Meet-Pune/6.jpg" class="panel2-img" alt="img" />
+            </div>
+          </div>
+
+        </div>
+      </details>
+
+
+      <details class="Alumni-panel2">
+        <summary>  Alumni Meet 2017 </summary>
+        <div class="panel2-row">
+          <div class="panel2-imgbox">
+            <div class="thumbnail">
+              <img src="http://tgpcet.com/assets/img/ALUMNI/Alumni-Meet-2017/1.jpg" class="panel2-img" alt="img" />
+            </div>
+          </div>
+          <div class="panel2-imgbox">
+            <div class="thumbnail">
+              <img src="http://tgpcet.com/assets/img/ALUMNI/Alumni-Meet-2017/2.jpg" class="panel2-img" alt="img" />
+            </div>
+          </div>
+          <div class="panel2-imgbox">
+            <div class="thumbnail">
+              <img src="http://tgpcet.com/assets/img/ALUMNI/Alumni-Meet-2017/3.jpg" class="panel2-img" alt="img" />
+            </div>
+          </div>
+        </div>
+      </details>
+
     </div>
   )
 }
