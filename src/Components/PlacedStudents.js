0661382890e0c439ab0773b304e0efb66743fbcf
Ritik0706 <ritiklg76@gmail.com>
2022-11-09 14:49:52 +0530

    Create placed student file

---
+++ src/Components/PlacedStudents.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import './PlacedStudents.css';
+
+function PlacedStudents() {
+    return (
+        <div class="placedStudent">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">Sr. No.</th>
+                        <th scope="col">Student Name</th>
+                        <th scope="col">Department</th>
+                        <th scope="col">Company Name</th>
+                        <th scope="col">Package</th>
+                        <th scope="col">LinkedIn Profile</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Ritik Gaikwad</td>
+                        <td>IT</td>
+                        <td>Google</td>
+                        <td>12Lakhs</td>
+                        <td><a href='https://www.linkedin.com/in/ritik-gaikwad-645957219'>View Profile</a></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default PlacedStudents
