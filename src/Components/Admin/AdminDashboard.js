268fc34dca8995fd22aca882fe4a19ab1f7b4f3a
Ritik0706 <ritiklg76@gmail.com>
2023-03-01 16:53:44 +0530

    btn design updated

--- src/Components/Admin/AdminDashboard.js
+++ src/Components/Admin/AdminDashboard.js
@@ -6,32 +6,46 @@ import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import HourglassEmptyIcon from '@mui/icons-material/HourglassEmpty';
 import PermIdentityIcon from '@mui/icons-material/PermIdentity';
 import ContentPasteSearchIcon from '@mui/icons-material/ContentPasteSearch';
-import { Link ,NavLink} from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
+import { logOut } from '../../Firebase';
 
 
 function Admindashboard() {
+  const navigate = useNavigate();
+  const handleLogout = async () => {
+
+    try {
+      await logOut();
+      alert("Logged Out Successfully");
+      navigate('/');
+    }
+    catch (error) {
+      alert(error.message)
+    }
+  }
+
   return (
     <div>
       <div className='bar'>
-      <Link to='/'> <ArrowBackIcon /></Link>
+        <Link to='/'> <ArrowBackIcon /></Link>
       </div>
       <div className='student'>
         <div className='sidebar'>
-          <h4 style={{color:'darkcyan'}}>DashBoard</h4>
+          <h4 style={{ color: 'darkcyan' }}>DashBoard</h4>
           <div>
-           <NavLink className='link' to=''> < PermIdentityIcon /> &nbsp; &nbsp;&nbsp;Admin Details </NavLink>
+            <NavLink className='link' to=''> < PermIdentityIcon /> &nbsp; &nbsp;&nbsp;Admin Details </NavLink>
           </div>
           <div>
-          <NavLink className='link' to='/fullCalender'><CalendarMonthIcon /> &nbsp;&nbsp;&nbsp;&nbsp;Full Calendar</NavLink> 
+            <NavLink className='link' to='/fullCalender'><CalendarMonthIcon /> &nbsp;&nbsp;&nbsp;&nbsp;Full Calendar</NavLink>
           </div>
           <div>
-           <NavLink  className='link' to='/exams'> <ContentPasteIcon/> &nbsp; &nbsp;&nbsp;Exams </NavLink>
+            <NavLink className='link' to='/exams'> <ContentPasteIcon /> &nbsp; &nbsp;&nbsp;Exams </NavLink>
           </div>
           <div>
-           <NavLink className='link' to='/drives'><HourglassEmptyIcon /> &nbsp; &nbsp;&nbsp;Drives </NavLink> 
+            <NavLink className='link' to='/drives'><HourglassEmptyIcon /> &nbsp; &nbsp;&nbsp;Drives </NavLink>
           </div>
           <div>
-            <NavLink className='link' to='/test'><ContentPasteSearchIcon /> &nbsp; &nbsp;&nbsp;Test </NavLink> 
+            <NavLink className='link' to='/test'><ContentPasteSearchIcon /> &nbsp; &nbsp;&nbsp;Test </NavLink>
           </div>
         </div>
 
@@ -69,6 +83,7 @@ function Admindashboard() {
             < div className='profile'>
               <button className='btn-primary  profile-btn'>Edit Profile</button>
               <button className='btn-primary  profile-btn'>Complete your Profile</button>
+              <button className='btn-primary  profile-btn' onClick={handleLogout}>Logout</button>
             </div>
           </div>
         </div>
