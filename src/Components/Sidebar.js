2e991f5f289bde341f47a3960f2f96ded804ce83
Bharti <bhartinagpure2409@gmail.com>
2023-04-10 13:05:34 +0530

    alice carousal added

---
+++ src/Components/Sidebar.js
@@ -0,0 +1,14 @@
+
+import React, { useState } from 'react';
+import './Sidebar.css';
+
+function Sidebar() {
+  return (
+    <div className="main-sidebar">
+     <div className='sidebar'> </div>
+     <div className='menu'></div>
+    </div>
+  )
+}
+
+export default Sidebar
