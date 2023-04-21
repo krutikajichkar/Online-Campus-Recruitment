007bcaeeb6e52fe3c06c360cc5aee4f2728241b5
Krutika Jichkar <93243210+krutikajichkar@users.noreply.github.com>
2023-04-21 11:10:56 +0530

    .

--- src/Components/Admin/AdminDashboard.js
+++ src/Components/Admin/AdminDashboard.js
@@ -24,7 +24,7 @@ const handleLogout = async () => {
       alert("Logged Out Successfully");
       navigate("/");
     } else {
-      navigate("/studentdashboard");
+      navigate("/admindashboard");
     }
   } catch (error) {
     alert(error.message);
